30cd6819a46efb0fac113256b25275a995fa4f37
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-04-12 12:29:45 +0300

    Update vector.cpp

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -5,40 +5,56 @@
 #include <cstring>
 #include <cmath>
 
-MyVector::MyVector(size_t size, ResizeStrategy, float coef)
+MyVector::MyVector(size_t size, ResizeStrategy ResizeStrategy, float coef, size_t delta)
 {
     _size = size;
     _capacity = 1;
     _data = nullptr;
-    _coef = coef;
+
+    _strategy = ResizeStrategy;
+    if (_strategy == ResizeStrategy::Additive)
+        _delta = delta;
+    if (_strategy == ResizeStrategy::Multiplicative)
+        _coef = coef;
 }
 
-MyVector::MyVector(size_t size, ValueType value, ResizeStrategy, float coef)
+MyVector::MyVector(size_t size, ValueType value, ResizeStrategy ResizeStrategy, float coef, size_t delta)
 {
-//    std::cout << "1" << std::endl;
+
     _capacity = size * coef;
     _size = size;
     _data = new ValueType[_capacity];
-//    std::cout << "2" << std::endl;
+
     for (size_t i = 0; i < _size; i++)
     {
-//        std::cout << i << std::endl;
+
         _data[i] = value;
     }
-    _coef = coef;
+
+    _strategy = ResizeStrategy;
+
+    if (_strategy == ResizeStrategy::Additive)
+        _delta = delta;
+    if (_strategy == ResizeStrategy::Multiplicative)
+        _coef = coef;
 }
 
 MyVector::MyVector(const MyVector& copy)
 {
-//    std::cout << "1" << std::endl;
+
     _size = copy._size;
-//    std::cout << "2" << std::endl;
+
     _capacity = copy._capacity;
-//    std::cout << "3" << std::endl;
+
     _data = new ValueType[_capacity];
-//    std::cout << "4" << std::endl;
+
     memcpy(_data, copy._data, sizeof(ValueType)*(_size));
-    _coef = copy._coef;
+
+    _strategy = copy._strategy;
+    if (_strategy == ResizeStrategy::Additive)
+        _delta = copy._delta;
+    if (_strategy == ResizeStrategy::Multiplicative)
+        _coef = copy._coef;
 }
 
 MyVector& MyVector::operator=(const MyVector& copy)
@@ -51,8 +67,13 @@ MyVector& MyVector::operator=(const MyVector& copy)
     this->_size = bufVector._size;
     this->_capacity = bufVector._capacity;
     memcpy(this->_data, copy._data, this->_size * sizeof (ValueType));
-    this->_coef = bufVector._coef;
-    std::cout << "Worked operator = " << std::endl;
+
+    this->_strategy = bufVector._strategy;
+    if (_strategy == ResizeStrategy::Additive)
+        _delta = bufVector._delta;
+    if (_strategy == ResizeStrategy::Multiplicative)
+        _coef = bufVector._coef;
+
     return *this;
 }
 
@@ -65,7 +86,7 @@ MyVector::~MyVector()
     }
     _size = 0;
     _capacity = 0;
-    std::cout << "Removing" << std::endl;
+//    std::cout << "Removing" << std::endl;
 }
 
 size_t MyVector::capacity() const
@@ -96,12 +117,20 @@ ValueType& MyVector::operator[](const size_t i) const
 
 void MyVector::reserve(const size_t capacity)
 {
-    if (loadFactor() < 1)
-        return;
+//    if (loadFactor() < 1)
+//        return;
 
     _capacity = (_capacity == 0) ? 1 : _capacity;
-    while(_capacity < capacity)
-        _capacity <<= 1;
+//    if (_strategy == ResizeStrategy::Additive)
+//    {
+
+//    }
+//    if (_strategy == ResizeStrategy::Multiplicative)
+//    {
+
+//    }
+    _capacity = capacity;
+    std::cout << " new Cap " << _capacity << std::endl;
 
     if (_data == nullptr)
         _data = new ValueType[_capacity];
@@ -118,42 +147,31 @@ void MyVector::pushBack(const ValueType &value)
 {
     if (loadFactor() == 1)
     {
-        reserve(_capacity * _coef);
+        if (_strategy == ResizeStrategy::Multiplicative)
+            reserve(_capacity * _coef);
+        if (_strategy == ResizeStrategy::Additive)
+            reserve(_capacity+_delta);
     }
-    ValueType *copy = new ValueType[_size + 1];
-    memcpy(copy, this->_data, _size * sizeof(ValueType));
-    copy[_size] = value;
-    if (this->_data != nullptr)
-        delete[] this->_data;
-    this->_data = copy;
+    this->_data[_size] = value;
     _size++;
     return;
 }
 
-//void MyVector::pushFront(const ValueType &value)
-//{
-//    ValueType *copy = new ValueType[_size + 1];
-//    copy[0] = value;
-//    for (size_t i = 0; i < _size; i++)
-//    {
-//        copy[i+1] = this->_data[i];
-//    }
-//    _size++;
-//    return;
-//}
+
 
 void MyVector::insert(const size_t i, const ValueType &value)
 {
     if (i > _size)
         return;
+ //       std::cout << _size << " " << _capacity;
 
-    if (loadFactor() == 1)
-        reserve(_size * _coef);
-//    if (i == 0)
-//    {
-//        pushFront(value);
-//        return;
-//    }
+    if (loadFactor() >= 1)
+    {
+        if (_strategy == ResizeStrategy::Multiplicative)
+            reserve(_size * _coef);
+        if (_strategy == ResizeStrategy::Additive)
+            reserve(_capacity+_delta);
+    }
 
     ValueType *copy = new ValueType[this->_size + 1];
     memcpy(copy, this->_data, sizeof(ValueType) * i);
@@ -177,8 +195,26 @@ void MyVector::insert(const size_t i, const MyVector &value)
     if (i > _size)
         return;
     _size += value._size;
+//    std::cout << _size << " " << _capacity;
     if (loadFactor() > 1)
-        reserve(_size * _coef);
+    {
+        if (_strategy == ResizeStrategy::Additive)
+        {
+            while (loadFactor() > 1)
+            {
+                reserve(_capacity + _delta);
+            }
+        }
+        if (_strategy == ResizeStrategy::Multiplicative)
+        {
+            while (loadFactor() > (1 / (_coef)*(_coef)))
+            {
+                reserve(_capacity * _coef);
+                    std::cout << "num1: " << capacity() << " " << size() << std::endl;
+            }
+        }
+
+    }
     _size -= value._size;
     ValueType *copy = new ValueType[this->_size + value._size];
     memcpy(copy, this->_data, sizeof(ValueType)*i);
@@ -266,33 +302,40 @@ void MyVector::resize(const size_t size, const ValueType value)
 {
     if (size > _size)
     {
-
         size_t oldSize = _size;
         _size = size;
-
         if (loadFactor() > 1)
         {
-//            std::cout << "loadFactor " << loadFactor() << std::endl;
-            _size = oldSize;
-            reserve(size * _coef);
-            _size = size;
-        }
-        ValueType *copy = new ValueType[oldSize];
-        memcpy(copy, this->_data, sizeof(ValueType) * oldSize);
+            if (_strategy == ResizeStrategy::Additive)
+            {
+                size_t newDelta = size - _capacity;
+                reserve(_capacity + newDelta);
+            }
 
-        delete[] this->_data;
+            if (_strategy == ResizeStrategy::Multiplicative)
+            {
+                reserve(size);
+            }
+            std::cout << _size << " - " << _capacity << std::endl;
+        }
+
+        ValueType *copy = new ValueType[oldSize];
+        if (_data != nullptr)
+        {
+            memcpy(copy, this->_data, sizeof(ValueType) * oldSize);
+
+            delete[] this->_data;
+        }
 
         _data = new ValueType[_size];
         for (size_t i = 0; i < oldSize; i++)
         {
             _data[i] = copy[i];
- //           std::cout << _data[i] << std::endl;
+
         }
         for (size_t i = oldSize; i < _size; i++)
         {
             _data[i] = value;
- //           std::cout << _data[i] << std::endl;
-
         }
     }
 
@@ -323,27 +366,4 @@ ValueType* MyVector::begin()
 ValueType* MyVector::end()
 {
     return this->_data+size();
-}
-
-
-MyVector MyVector::sortedSquares(const MyVector &vec, bool SortedStrategy)
-{
-    MyVector sorted; //возвращаемое значение
-    sorted._coef = vec._coef;
-    sorted._size = vec._size;
-    sorted._capacity = vec._capacity;
-    sorted._data = new ValueType[vec._capacity];
-//буду сравнивать первое и последнее число из массива, второе и предпоследнее и тд, но это не точно
-    //возможно стоит сравнивать самое маленькое по модулю отрицательное с самым маленьким неотриц и тд, двигаясь к краям
-    //тогда придется отдельно писать функции для случаев, где все числа отриц и неотриц
-
-
-
-
-    std::cout << "Begin" << std::endl;
-    for (ValueType *i = sorted.begin(); i < sorted.end(); i++)
-        std::cout << *i << ", ";
-    std::cout << "End" << std::endl;
-
-    return  sorted;
 }
