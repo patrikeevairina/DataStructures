f78e4ae4c3ae24af24b5cab30060ca8658372c24
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-04-12 15:33:57 +0300

    Update List.h

--- LinkedList/List.h
+++ LinkedList/List.h
@@ -1,3 +1,4 @@
+#ifndef LIST_H
 #define LIST_H
 
 #include <iostream>
@@ -13,11 +14,11 @@ class List
     // поле с ключем в узел и, с учетом этого, поменять методы LinkedList
     // (доступ по ключу, поиск по ключу и т.д.)
     struct Node {
-        Node(const ValueType& value, Node* next = nullptr, Node *previous = nullptr);//+
+        Node(const ValueType& value, Node* next = nullptr, Node *previous = nullptr);//
         ~Node();
 
-        void insertNext(const ValueType& value);//+
-        void removeNext();//+
+        void insertNext(const ValueType& value);//
+        void removeNext();//
 
         ValueType value;
         Node* next;
@@ -25,36 +26,35 @@ class List
     };
 
 public:
-    ////
-    List();//+
-    List(const List& copyList);//+
-    List& operator=(const List& copyList);//какая-то приколюха, не работает пока
+    
+    List();//
+    List(const List& copyList);//
+    List& operator=(const List& copyList);/
 
-    List(List&& moveList) noexcept;//это же для умных
-    List& operator=(List&& moveList) noexcept;
+
 
     ~List();
     ////
 
     // доступ к значению элемента по индексу
-    ValueType& operator[](const size_t pos) const;//+
+    ValueType& operator[](const size_t pos) const;//
     // доступ к узлу по индексу
-    List::Node* getNode(const size_t pos) const;//+
+    List::Node* getNode(const size_t pos) const;//
 
     // вставка элемента по индексу, сначала ищем, куда вставлять (О(n)), потом вставляем (O(1))
-    void insert(const size_t pos, const ValueType& value);//++
+    void insert(const size_t pos, const ValueType& value);//
     // вставка элемента после узла, (O(1))
     static void insertAfterNode(Node* node, const ValueType& value);
     // вставка в конец (О(n))
-    void pushBack(const ValueType& value);//+
+    void pushBack(const ValueType& value);//
     // вставка в начало (О(1))
-    void pushFront(const ValueType& value);//+
+    void pushFront(const ValueType& value);//
 
     // удаление
-    void remove(const size_t pos);//i think it's okay but i'm not sure
-    void removeNextNode(Node* node);//it must be okay
-    void removeFront();//+
-    void removeBack();//+, but strange if _size == 1
+    void remove(const size_t pos);//
+    void removeNextNode(Node* node);//
+    void removeFront();//
+    void removeBack();//
 
     // поиск, О(n)
     long long int findIndex(const ValueType& value) const;
@@ -62,8 +62,8 @@ public:
 
     // разворот списка
     void reverse();		//+				// изменение текущего списка
-    List reverse1() const;			// полчение нового списка (для константных объектов)// не хватило мозгов на осмысление этого метода
-    List getReverseList() const;	// чтобы неконстантный объект тоже мог возвращать новый развернутый список//на осмысление этого тоже не хватило:(
+    List reverse1() const;			// полчение нового списка (для константных объектов)// 
+    List getReverseList();	// чтобы неконстантный объект тоже мог возвращать новый развернутый список//
 
     size_t size() const;
 private:
