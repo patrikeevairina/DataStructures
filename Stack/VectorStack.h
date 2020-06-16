d1498e6c97c79179f54ef44262f4ccecd4d55d5b
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-17 00:03:59 +0300

    нужный хедер

--- Stack/VectorStack.h
+++ Stack/VectorStack.h
@@ -18,7 +18,26 @@ void VectorStack::pop()
 ValueType& VectorStack::top()
 {
     return (*this)[size()-1];
-}
+}#pragma once
+#include "StackImplementation.h"
+#include "MyVector.h"
+
+class VectorStack : public StackImplementation, public MyVector
+{
+public:
+    VectorStack() {}
+    VectorStack(const VectorStack& copyVector);
+    void push(const ValueType &value) override;
+    void pop() override;
+    ValueType & top() override;
+    const ValueType & top() const override;
+    bool isEmpty() const override;
+    size_t size() const override;
+
+    ~VectorStack() override {};
+};
+
+
 
 const ValueType& VectorStack::top() const
 {
