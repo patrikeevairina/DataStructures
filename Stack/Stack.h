ac0be2b0a26e234ef18378f4aa3e30a8f0bedf47
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-16 15:09:55 +0300

    Update Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,53 +1,35 @@
+#include <iostream>
+#include "ListStack.h"
+#include "VectorStack.h"
+#include "StackImplementation.h"
+
 #pragma once
-// Уровень абстракции
-// клиентский код подключает именно этот хедер
 
-// тип значений в стеке
-using ValueType = double;
-
-// на основе какого контейнера работает стек
 enum class StackContainer {
-	Vector = 0,
-	List,
-	// можно дополнять другими контейнерами
+    Vector = 0,
+    List,
 };
-
-// декларация класса с реализацией
-class StackImplementation;
 
 class Stack
 {
 public:
-	// Большая пятерка
-	Stack(StackContainer container = StackContainer::Vector);
-	// элементы массива последовательно подкладываются в стек
-	Stack(const ValueType* valueArray, const size_t arraySize, 
-		  StackContainer container = StackContainer::Vector);
-
-	explicit Stack(const Stack& copyStack);
-	Stack& operator=(const Stack& copyStack);
-
-	// Здесь как обычно
-	// Stack(Stack&& moveStack) noexcept;
-	// Stack& operator=(Stack&& moveStack) noexcept;
-
-	~Stack();
-
-	// добавление в хвост
-	void push(const ValueType& value);
-	// удаление с хвоста
-	void pop();
-	// посмотреть элемент в хвосте
-	ValueType& top();
-	const ValueType& top() const;
-	// проверка на пустоту
-	bool isEmpty() const;
-	// размер 
-	size_t size() const;
+    Stack(StackContainer container = StackContainer::List);
+    Stack(const ValueType* valueArray, const size_t arraySize,
+          StackContainer container = StackContainer::List);
+    explicit Stack(const Stack& copyStack);
+    Stack& operator=(const Stack& copyStack);
+    Stack(Stack&& moveStack) noexcept;
+    Stack& operator=(Stack&& moveStack) noexcept;
+    ~Stack();
+    
+    void push(const ValueType& value);
+    void pop();
+    ValueType& top();
+    const ValueType& top() const;
+    bool isEmpty() const;
+    size_t size() const;
 private:
-	// указатель на имплементацию (уровень реализации)
-	StackImplementation* _pimpl = nullptr;
-	// тип контейнера, наверняка понадобится
-	StackContainer _containerType;
+    StackImplementation* _pimpl = nullptr;
+    StackContainer _containerType;
 };
 
