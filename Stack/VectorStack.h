6198a12b05adf098fa1c8057318d0e2eef098602
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-16 17:27:21 +0300

    Update VectorStack.h

--- Stack/VectorStack.h
+++ Stack/VectorStack.h
@@ -1,19 +1,39 @@
-#pragma once
-#include "StackImplementation.h"
-#include "MyVector.h"
+#include "VectorStack.h"
 
-class VectorStack : public StackImplementation, public MyVector
+VectorStack::VectorStack(const VectorStack &copy) : MyVector(copy)
+{}
+
+void VectorStack::push(const ValueType &value)
 {
-public:
-    VectorStack() {}
-    VectorStack(const VectorStack& copyVector);
-    void push(const ValueType &value) override;
-    void pop() override;
-    ValueType & top() override;
-    const ValueType & top() const override;
-    bool isEmpty() const override;
-    size_t size() const override;
+    this->pushBack(value);
+}
 
-    ~VectorStack() override {};
-};
+void VectorStack::pop()
+{
+    if (isEmpty())
+        throw std::out_of_range("stack is empty");
+    this->popBack();
+}
 
+ValueType& VectorStack::top()
+{
+    return (*this)[size()-1];
+}
+
+const ValueType& VectorStack::top() const
+{
+    return (*this)[size()-1];
+}
+
+bool VectorStack::isEmpty() const
+{
+    if (size() == 0)
+        return true;
+    else
+        return false;
+}
+
+size_t VectorStack::size() const
+{
+    return this->MyVector::size();
+}
