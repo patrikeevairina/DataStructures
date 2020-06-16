194466a17b2ede3e3ec69e6a3f4d1922feca6b84
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-16 17:26:33 +0300

    Update VectorStack.cpp

--- Stack/VectorStack.cpp
+++ Stack/VectorStack.cpp
@@ -1,2 +1,19 @@
-#include "VectorStack.h"
+#pragma once
+#include "StackImplementation.h"
+#include "MyVector.h"
+
+class VectorStack : public StackImplementation, public MyVector
+{
+public:
+    VectorStack() {}
+    VectorStack(const VectorStack& copyVector);
+    void push(const ValueType &value) override;
+    void pop() override;
+    ValueType & top() override;
+    const ValueType & top() const override;
+    bool isEmpty() const override;
+    size_t size() const override;
+
+    ~VectorStack() override {};
+};
 
