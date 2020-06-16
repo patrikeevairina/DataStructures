e8f4350d621e5406c160bd14fb9e4a2bb08017e1
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-16 14:07:07 +0300

    последняя версия

--- Stack/ListStack.cpp
+++ Stack/ListStack.cpp
@@ -1,1 +1,39 @@
+#pragma once
 #include "ListStack.h"
+
+ListStack::ListStack(const ListStack &copy) : LinkedList(copy) {}
+
+void ListStack::push(const ValueType &value)
+{
+    this->pushFront(value);
+}
+
+void ListStack::pop()
+{
+    if (isEmpty())
+        throw std::out_of_range("stack is empty");
+    this->removeFront();
+}
+
+ValueType& ListStack::top()
+{
+    return getNode(0)->value;
+}
+
+const ValueType& ListStack::top() const
+{
+    return getNode(0)->value;
+}
+
+bool ListStack::isEmpty() const
+{
+    if (size() == 0)
+        return true;
+    else
+        return false;
+}
+
+size_t ListStack::size() const
+{
+    return this->LinkedList::size();
+}
