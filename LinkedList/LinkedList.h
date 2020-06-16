8b9929a0d0eddc05ab553e8f70fa763a592c371e
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-16 14:58:27 +0300

    Update LinkedList.h

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -10,66 +10,53 @@ class LinkedList
     struct Node {
         Node(const ValueType& value, Node* next = nullptr);
         ~Node();
-
+        
         void insertNext(const ValueType& value);
         void removeNext();
-
+        
         ValueType value;
         Node* next;
     };
 private:
     Node*	_head;
     size_t	_size;
-
+    
     void forceNodeDelete(Node* node);
-
+    
 public:
-
+    
     LinkedList();
     LinkedList(const LinkedList& copyList);
     LinkedList& operator=(const LinkedList& copyList);
-
+    
     LinkedList(LinkedList&& moveList) noexcept;
     LinkedList& operator=(LinkedList&& moveList) noexcept;
-
+    
     ~LinkedList();
-
-
-    // доступ к значению элемента по индексу
+    
     const ValueType& operator[](const size_t pos) const;
     ValueType& operator[](const size_t pos);
-    // доступ к узлу по индексу
     LinkedList::Node* getNode(const size_t pos) const;
     LinkedList::Node* getNode(const size_t pos);
-
-    // вставка элемента по индексу, сначала ищем, куда вставлять (О(n)), потом вставляем (O(1))
+    
     void insert(const size_t pos, const ValueType& value);
-    // вставка элемента после узла, (O(1))
     void insertAfterNode(Node* node, const ValueType& value);
-    // вставка в конец (О(n))
     void pushBack(const ValueType& value);
-    // вставка в начало (О(1))
     void pushFront(const ValueType& value);
-
-    // удаление
+    
     void remove(const size_t pos);
     void removeNextNode(Node* node);
     void removeFront();
     void removeBack();
-
-    // поиск, О(n)
+    
     long long int findIndex(const ValueType& value) const;
     Node* findNode(const ValueType& value) const;
-
-    // разворот списка
-    void reverse();						// изменение текущего списка
-    LinkedList reverse() const;			// полчение нового списка (для константных объектов)
-    LinkedList getReverseList() ;	// чтобы неконстантный объект тоже мог возвращать новый развернутый список
-
+    
+    void reverse();		
+    LinkedList reverse() const;	
+    LinkedList getReverseList();	
+    
     size_t size() const;
-
 };
-
-
 
 #endif // LINKEDLIST_H
