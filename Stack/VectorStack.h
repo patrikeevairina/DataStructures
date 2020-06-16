cf87544e136ffaa315d0995598ff5f8921708263
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-17 00:04:12 +0300

    Update VectorStack.h

--- Stack/VectorStack.h
+++ Stack/VectorStack.h
@@ -1,24 +1,4 @@
-#include "VectorStack.h"
-
-VectorStack::VectorStack(const VectorStack &copy) : MyVector(copy)
-{}
-
-void VectorStack::push(const ValueType &value)
-{
-    this->pushBack(value);
-}
-
-void VectorStack::pop()
-{
-    if (isEmpty())
-        throw std::out_of_range("stack is empty");
-    this->popBack();
-}
-
-ValueType& VectorStack::top()
-{
-    return (*this)[size()-1];
-}#pragma once
+#pragma once
 #include "StackImplementation.h"
 #include "MyVector.h"
 
@@ -37,22 +17,3 @@ public:
     ~VectorStack() override {};
 };
 
-
-
-const ValueType& VectorStack::top() const
-{
-    return (*this)[size()-1];
-}
-
-bool VectorStack::isEmpty() const
-{
-    if (size() == 0)
-        return true;
-    else
-        return false;
-}
-
-size_t VectorStack::size() const
-{
-    return this->MyVector::size();
-}
