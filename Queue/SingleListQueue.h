d5f07a7a019de67316a97de7e7a703a26fa06576
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-06-18 19:01:36 +0300

    Update SingleListQueue.h

--- Queue/SingleListQueue.h
+++ Queue/SingleListQueue.h
@@ -3,8 +3,8 @@
 #include "QueueImplementation.h"
 #include "LinkedList.h"
 
-
-class SingleListQueue : public QueueImplementation, public LinkedList
+template <typename ValueType>
+class SingleListQueue : public QueueImplementation<ValueType>, public LinkedList<ValueType>
 {
 public:
     SingleListQueue(){}
@@ -18,4 +18,48 @@ public:
     ~SingleListQueue() override {};
 };
 
+template <typename ValueType>
+SingleListQueue<ValueType>::SingleListQueue(const SingleListQueue& copy)
+    :LinkedList<ValueType> (copy){}
+
+template <typename ValueType>
+void SingleListQueue<ValueType>::enqueue(const ValueType& value)
+{
+    this->pushBack(value);
+}
+
+template <typename ValueType>
+void SingleListQueue<ValueType>::dequeue()
+{
+    if (this->isEmpty())
+        throw std::out_of_range("incorrect");
+    this->LinkedList<ValueType>::removeFront();
+}
+
+template <typename ValueType>
+ValueType& SingleListQueue<ValueType>::front()
+{
+    return (*this)[0];
+}
+
+template <typename ValueType>
+const ValueType& SingleListQueue<ValueType>::front() const
+{
+    return (*this)[0];
+}
+
+template <typename ValueType>
+bool SingleListQueue<ValueType>::isEmpty() const
+{
+    if (size() == 0)
+        return true;
+    else
+        return false;
+}
+
+template <typename ValueType>
+size_t SingleListQueue<ValueType>::size() const
+{
+    return this->LinkedList<ValueType>::size();
+}
 
